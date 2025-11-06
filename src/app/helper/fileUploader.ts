6a7f539edae2df94ce451ffbd448e98ee536da24
Asad Atik <asadatik1995@gmail.com>
2025-11-06 15:49:56 +0600

    user validation added

---
+++ src/app/helper/fileUploader.ts
@@ -0,0 +1,30 @@
+import multer from 'multer'
+import path from 'path'
+import {v2 as cloudinary} from 'cloudinary'
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,path.join(process.cwd(), 'uploads'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+
+
+
+const upload = multer({ storage: storage })
+
+const uploadToCloudinary = async (file: Express.Multer.File) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.path, {
+
+    })
+    return result.secure_url
+  } catch (error) {
+    throw new Error('Cloudinary upload failed')
+  }
+}
---
+++ src/modules/User/user.validation.ts
@@ -0,0 +1,85 @@
+import { Gender, UserRole, UserStatus } from "@prisma/client";
+import { z } from "zod";
+
+const createAdmin = z.object({
+    password: z.string({
+        error: "Password is required"
+    }),
+    admin: z.object({
+        name: z.string({
+            error: "Name is required!"
+        }),
+        email: z.string({
+            error: "Email is required!"
+        }),
+        contactNumber: z.string({
+            error: "Contact Number is required!"
+        })
+    })
+});
+
+const createDoctor = z.object({
+    password: z.string({
+        error: "Password is required"
+    }),
+    doctor: z.object({
+        name: z.string({
+            error: "Name is required!"
+        }),
+        email: z.string({
+            error: "Email is required!"
+        }),
+        contactNumber: z.string({
+            error: "Contact Number is required!"
+        }),
+        address: z.string().optional(),
+        registrationNumber: z.string({
+            error: "Reg number is required"
+        }),
+        experience: z.number().optional(),
+        gender: z.enum([Gender.MALE, Gender.FEMALE]),
+        appointmentFee: z.number({
+            error: "appointment fee is required"
+        }),
+        qualification: z.string({
+            error: "quilification is required"
+        }),
+        currentWorkingPlace: z.string({
+            error: "Current working place is required!"
+        }),
+        designation: z.string({
+            error: "Designation is required!"
+        })
+    })
+});
+
+const createPatient = z.object({
+    password: z.string(),
+    patient: z.object({
+        email: z.string({
+            error: "Email is required!"
+        }).email(),
+        name: z.string({
+            error: "Name is required!"
+        }),
+        contactNumber: z.string({
+            error: "Contact number is required!"
+        }),
+        address: z.string({
+            error: "Address is required"
+        })
+    })
+});
+
+const updateStatus = z.object({
+    body: z.object({
+        status: z.enum([UserStatus.ACTIVE, UserStatus.BLOCKED, UserStatus.DELETED])
+    })
+})
+
+export const userValidation = {
+    createAdmin,
+    createDoctor,
+    createPatient,
+    updateStatus
+}
